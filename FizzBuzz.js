e8d4477d3dce1fbb5f6dc86107f89c2424eb0e9f
eovillafuerte <eovillafuerte@outlook.com>
2022-09-30 17:29:02 -0600

    else if statement

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -6,3 +6,14 @@ function fizzBuzz() {
     else if (count % 3 === 0) {
         output.push("fizz")
         }
+        else if (count % 5 === 0) {
+            output.push("Buzz");
+        }
+        else {
+            output.push(count);
+        }
+
+        count++;
+
+            console.log()
+        }
